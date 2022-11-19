70203be288858036e5046d87fa042ddb2634b4a5
Jonah Evangelista <jonahevangelista@yahoo.com>
2022-11-18 23:31:48 -0700

    Initial Commit

---
+++ Days Map/Days_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Days_MapApp.swift
+//  Days Map
+//
+//  Created by Jonah Marie Evangelista on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Days_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
